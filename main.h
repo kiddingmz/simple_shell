c8a47e370449abf9140d16c8b76b66ef169d5b0d
kiddingmz <bata.isidro@gmail.com>
2023-07-16 17:06:58 +0200

    FEAT: interactive mode

--- main.h
+++ main.h
@@ -22,7 +22,7 @@ char *_strcat_path(char *, char *);
 void _strcat_path(const char *, const char *, char *);
 int check_path(char *);
 void non_interactive(int, char **, char **);
-/*void interactive(int, char **, char **);*/
+void interactive(int, char **, char **);
 /*int _strcmp(const char *, const char *);*/
 /*void _exe(int, char **, char *, char *, char **);*/
 /*void _free_array(char **);*/
